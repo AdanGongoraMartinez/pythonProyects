58fd903749b47a520c9bed5eddde5dcd95dc5191
Adan <adangonmar@gmail.com>
2023-12-15 22:29:09 -0600

    Adding a explanation to nombre_cerveza

--- day_1/nombre_cerveza.py
+++ day_1/nombre_cerveza.py
@@ -1,3 +1,6 @@
 #Proyecto para crear el nombre de una marca de cerbezas usando unicamente una linea
+
+#Uso de input y de la \
+
 print("El nombre de tu cerveza\nes '"+input("¿Qué ciudad te gustaría visitar?: ")+" "+input("¿Cuál es tu color favorito?: ")+"'\nFelicidades!")
 
